b0516f4ef2d6f88da712360397e2806e0dd81bfe
Rory How <me@rory.how>
2019-12-04 22:43:48 +0200

    Remove license and update Readme

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject adventofcode2019 "0.1.0-SNAPSHOT"
   :description "Advent of code 2019 edition"
   :url "https:://www.rory.how"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns adventofcode2019.core})
