9c2b3817bf52380369465251136a40f573ae134f
Rory How <me@rory.how>
2019-12-04 22:41:37 +0200

    Remove some things that I don't need

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject adventofcode2019 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Advent of code 2019 edition"
+  :url "https:://www.rory.how"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
