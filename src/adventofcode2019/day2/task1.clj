d1259a7f5b71099012e4e17f7acf0278a394c572
Rory How <me@rory.how>
2019-12-06 21:47:02 +0200

    Add day2 part 1

---
+++ src/adventofcode2019/day2/task1.clj
@@ -0,0 +1,24 @@
+(ns adventofcode.day2
+  (:require [clojure.string :as str]))
+
+(def OPCODE-LIST-STEP 4)
+(def opcodes (-> (slurp "src/adventofcode2019/day2/input.txt")
+                 str/trim-newline
+                 (str/split #",")
+                 vec-parse-int
+                 (assoc 1 12 2 2)))
+
+(defn intcode-program [opcodes]
+  (loop [codes opcodes
+         idx 0]
+    (let [[opcode p1 p2 r1] (subvec codes idx (+ idx OPCODE-LIST-STEP))]
+      (cond
+        (= opcode 99) codes
+        (= opcode 1) (recur (assoc codes r1 (+ (nth codes p1) (nth codes p2))) (+ idx OPCODE-LIST-STEP))
+        (= opcode 2) (recur (assoc codes r1 (* (nth codes p1) (nth codes p2))) (+ idx OPCODE-LIST-STEP))))))
+
+(defn vec-parse-int [list]
+  (vec (map #(Integer/parseInt %) list)))
+
+(defn task1 []
+  (nth (intcode-program opcodes) 0))
